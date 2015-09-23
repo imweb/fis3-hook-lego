94a4fc692f464f254d51317bdf616224813600c1
jiangyuan <jiangyuanlucky@gmail.com>
2015-09-23 21:55:17 +0800

    refact package.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /**
-export CLICOLOR=1
- * lego mmodule hook
+ * lego module hook
  * @author jerojiang
  * @link http://lego.imweb.io/
  */
@@ -9,7 +8,6 @@ var pack = require('./lib/pack');
 
 module.exports = function init(fis, opts) {
     fis.on('lookup:file', function(info, file) {
-
         // 暂时只分析 js 文件，后续 Ques 可以这里搞起
         if (file.isJsLike && /^[a-zA-Z0-9_@.-]+$/.test(info.rest)) {
             var ret = lookup(info.rest);
