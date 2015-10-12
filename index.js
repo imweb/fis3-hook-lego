992a4c3080bdf2934a1dfedd02108a629aac5985
helondeng <dhlwust@163.com>
2015-10-12 16:10:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,20 @@
 var lookup = require('./lookup');
 
 module.exports = function init(fis, opts) {
+    fis.set('idMaps', {});
     fis.on('lookup:file', function(info, file) {
 
         // 暂时只分析 js 文件，后续 Ques 可以这里搞起
         if (file.isJsLike && /^[a-zA-Z0-9_@.-]+$/.test(info.rest)) {
             var ret = lookup(info.rest);
+            // hook 会修改文件的id，这里把这种修改记录下来，方便其他模块调用
+            var idMaps = fis.get('idMaps');
+            if(!idMaps[ret]) {
+                idMaps[ret] = info.rest;
+            }
+            
+            fis.set('idMaps', idMaps);
+            
             if (ret) {
                 info.id = info.moduleId = ret;
             }
