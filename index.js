d455360248eb7fc19d77f89733572db9de200b40
vienwu <vienwu@tencent.com>
2015-12-01 16:16:07 +0800

    add power support.

--- index.js
+++ index.js
@@ -4,12 +4,19 @@
  * @link http://lego.imweb.io/
  */
 var lookup = require('./lookup');
-
+var powerLego = require('./powerLego');
 var entry = module.exports = function(fis, opts) {
     fis.on('lookup:file', function(info, file) {
         // 不处理相对路径的文件和已经处理了的文件
         if (file.isJsLike && info.rest && info.rest[0] !== '.' && !info.id) {
             var ret = lookup(info.rest, opts);
+            if (ret && ret.file) {
+                info.id = ret.file.getId();
+                info.file = ret.file;
+            }
+        } else if (opts.powerLego && info.rest && powerLego.reg.test(info.rest) && !info.id) {
+            // 处理inline和样式
+            var ret = powerLego.lookup(info.rest, opts);
             if (ret && ret.file) {
                 info.id = ret.file.getId();
                 info.file = ret.file;
---
+++ powerLego.js
@@ -0,0 +1,81 @@
+var _ = fis.util,
+    fs = _.fs,
+    compare = require('compare-version'),
+    root = 'lego_modules';
+
+
+module.exports = {
+    reg: /^lego_modules\/([a-zA-Z0-9-_]+)(@\d+\.\d+\.\d+)?\/(.*)$/,
+    lookup: function (id, opts) {
+        var ver, versions,
+            match = id.match(this.reg),
+            lego = fis.project.getProjectPath(root),
+            pkgName = match && match[1] || '',
+            ver = match && match[2] || '',
+            subFile = match && match[3] || '';
+        // 做一次快速判断
+        if (!pkgName || !getListAll(root)[pkgName]) {
+            return null;
+        }
+
+        fis.log.debug('lego: get %s from <lego_modules>', id);
+
+        if (!_.isDir(_(lego, pkgName))) {
+            return fis.log.info('lego: 找不到 lego 组件 %s 的目录', id);
+        }
+
+        if (!ver) { // 默认最新
+            versions = fs.readdirSync(_(lego, pkgName)) || [];
+
+            if (versions.length > 1) {
+                versions = versions.sort(function (prev, cur) {
+                    return compare(prev, cur) <= 0;
+                });
+            }
+
+            ver = versions[0];
+        }
+
+
+        if (!_.isDir(_(lego, pkgName, ver))) {
+            return fis.log.info('lego: 找不到 lego 组件 %s 的对应的版本', id, ver);
+        }
+
+        if (!subFile) {
+            try {
+                subFile = require(_(lego, pkgName, ver, 'package.json'))
+                    .lego.main;
+            } catch (ex) {
+                // 如果不存在 package.json
+                fis.log.info('lego: 组件 %s 没有 package.json', id);
+            }
+
+
+        if (!_.isFile(_(lego, pkgName, ver, subFile))) {
+            return fis.log.error('lego: 找不到 lego 组件 %s 的对应的文件', id, subFile);
+        }
+
+        return fis.uri(_(root, pkgName, ver, subFile));
+    }
+};
+
+
+/**
+ * 每一个文件都会经过lookup，防止频繁的判断文件是否存在，先列出一级目录，以备快速判断
+ */
+var _listAll = {};
+
+function getListAll(root) {
+    var path;
+    if (!_listAll[root]) {
+        _listAll[root] = {};
+        path = fis.project.getProjectPath(root);
+        if (_.isDir(path)) {
+            fs.readdirSync(path).forEach(function(item) {
+                _listAll[root][item] = true;
+            });
+        }
+    }
+    return _listAll[root];
+};
+
