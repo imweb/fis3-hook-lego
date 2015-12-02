f2891d138b3a161003fae80033e954ffaabaf823
fishineyuan <382184760@qq.com>
2015-12-02 13:30:26 +0800

    fix 覆盖commonjs的查找结果

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ var lookup = require('./lookup');
 var powerLego = require('./powerLego');
 var entry = module.exports = function(fis, opts) {
     fis.on('lookup:file', function(info, file) {
-        // 不处理相对路径的文件和已经处理了的文件
-        if (file.isJsLike && info.rest && info.rest[0] !== '.' && !info.id) {
+        // 不处理相对路径的文件
+        // 不能跳过已经有info.id的文件，必须处理
+        //    例如: abc.js中require('abc') CommonJs的lookup会找到自己,
+        //      并不一定需要require('./abc'), 此时如果是想要lego_modules下的abc就惨了
+        if (file.isJsLike && info.rest && info.rest[0] !== '.') {
             var ret = lookup(info.rest, opts);
             if (ret && ret.file) {
                 info.id = ret.file.getId();
+                info.moduleId = ret.file.moduleId;
                 info.file = ret.file;
             }
         } else if (opts.powerLego && info.rest && powerLego.reg.test(info.rest) && !info.id) {
